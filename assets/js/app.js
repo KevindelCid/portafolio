025a88100e685d7f5bb3c42bb6dd38f560d2424c
Kevin del Cid <danieldelcidf@gmail.com>
2022-08-13 22:22:56 -0600

    Se mejoró el codigo js, se agregÃaron animaciones y una mejor experiencia de usuario

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,46 @@
 const menu = document.getElementById("menu");
 const btnMenu = document.getElementById("btnMenu");
-let menuState = false;
-let scrollMenuState = false;
-const body = document.getElementById("body");
-function displayMenu() {
-  if (!menuState) {
-    menu.style.left = "0px";
-    btnMenu.textContent = "X";
-    menuState = true;
-  } else {
-    menu.style.left = "-1000rem";
-    menuState = false;
-    btnMenu.textContent = "☰";
-  }
-}
+const anchor = document.getElementsByClassName("anchor-menu");
 
-function displayMenuOff() {
-  if (menuState) {
-    menu.style.left = "-1000rem";
-    menuState = false;
+window.addEventListener("load", (e) => {
+  screen.width > 1149 ? displayMenu() : hiddenMenu();
+});
+
+/*
+// no funciona... saber por qué jaja
+  anchor.document.addEventListener("click", (e) => {
+    console.log(e);
+  });
+*/
+
+const displayMenu = () => {
+  menu.classList.contains("hide-menu") ? menuDisplayer() : hiddenMenu();
+};
+
+const menuDisplayer = () => {
+  menu.classList.replace("hide-menu", "display-menu");
+  btnMenu.classList.replace("change-x-burger", "change-burger-x");
+  setTimeout(() => {
+    btnMenu.textContent = "X";
+  }, 250);
+};
+const hiddenMenu = () => {
+  menu.classList.replace("display-menu", "hide-menu");
+  btnMenu.classList.replace("change-burger-x", "change-x-burger");
+
+  setTimeout(() => {
     btnMenu.textContent = "☰";
+  }, 250);
+};
+
+window.addEventListener("resize", (e) => {
+  screen.width > 1149 ? menuDisplayer() : hiddenMenu();
+});
+
+window.onscroll = function () {
+  if (screen.width > 1149) {
+    window.scrollY > 10
+      ? menu.classList.add("background--dinamic")
+      : menu.classList.remove("background--dinamic");
   }
-}
+};
